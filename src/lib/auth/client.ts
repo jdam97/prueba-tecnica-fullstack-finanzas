a4efa7116bc1d1d388a9832f8bb588f80d21eb13
Jdam <jonathandalvarez7@gmail.com>
2025-07-25 23:43:06 -0500

    feat: :sparkles: add auth configuration

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,5 +1,23 @@
-import { createAuthClient } from 'better-auth/react';
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
 });
+
+export const { useSession, signIn, signOut } = authClient;
+
+export const useSessionWithRole = () => {
+  const session = useSession()
+  return {
+    ...session,
+    data: session.data
+      ? {
+          ...session.data,
+          user: {
+            ...session.data.user,
+            role: (session.data.user as any).role as string,
+          },
+        }
+      : null,
+  }
+}
--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,17 +1,51 @@
+// src/lib/auth/index.ts
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { prisma } from '@/lib/auth/prisma'; 
+import { prisma } from '@/lib/auth/prisma';
 
-export const auth = betterAuth({
+const auth = betterAuth({
   database: prismaAdapter(prisma, {
-  provider: 'postgresql',
+    provider: 'postgresql',
+    user: {
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        image: true,
+        emailVerified: true,
+        createdAt: true,
+        updatedAt: true,
+        role: true,
+      }
+    }
   }),
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  callbacks: {
+    async session({ session, user }:any) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          role: user.role as "ADMIN" | "USER",
+        },
+      }
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "ADMIN",
+        required: false,
+      },
     },
   },
 });
 
-export type Session = typeof auth.$Infer.Session;
+export default auth;
+
