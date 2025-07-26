fb79b6578e730dcde88cd3783e1c910675cd6ceb
Jdam <jonathandalvarez7@gmail.com>
2025-07-25 23:43:44 -0500

    feat: :sparkles: add controllers

---
+++ controllers/transaction.controller.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getTransactionsService, createTransactionService, deleteTransactionService } from "../services/transaction.service"
+
+
+//Get all transactions services(ADMIN)
+export async function getTransactionsController(req: NextApiRequest, res: NextApiResponse) {
+  const { userId } = req.query;
+  try {
+    const transactions = await getTransactionsService(userId as string | undefined);
+    return res.status(200).json(transactions);
+  } catch (error) {
+    return res.status(500).json({ error: "Error al obtener movimientos" });
+  }
+}
+
+//create transactions(USER-ADMIN)
+export async function createTransactionController(req: NextApiRequest, res: NextApiResponse) {
+    const { amount, concept, date, userId, type } = req.body;
+  
+    if (!amount || !concept || !date || !userId || !type) {
+      return res.status(400).json({ error: "Todos los campos son obligatorios" });
+    }
+  
+    try {
+      const newTransaction = await createTransactionService({ amount, concept, date, userId, type });
+      return res.status(201).json(newTransaction);
+    } catch (error) {
+      console.error(" Error creando transacción:", error);
+      return res.status(500).json({ error: "Error al crear el movimiento" });
+    }
+  }
+
+  //delete transaction(ADMIN)
+  export async function deleteTransactionController(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+    if (typeof id !== "string") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+  
+    try {
+      await deleteTransactionService(id);
+      return res.status(200).json({ message: "Movimiento eliminado correctamente" });
+    } catch (error) {
+      return res.status(500).json({ error: "Error al eliminar movimiento" });
+    }
+  }
---
+++ controllers/user.controller.ts
@@ -0,0 +1,30 @@
+import { any } from "better-auth";
+import { getAllUsersService,editUserByIdService } from "../services/user.service";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+//Get all users Controller
+export async function getAllUsersController(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const users = await getAllUsersService();
+    return res.status(200).json(users);
+  } catch (e) {
+    return res.status(500).json({ error: "Error interno" });
+  }
+}
+
+export async function editUserByIdController(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  const { name, role, email } = req.body;
+
+  if (typeof id !== "string") {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const updatedUser = await editUserByIdService(id, { name, role, email });
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error editando usuario:", error);
+    return res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
