fe1353778c2528aa131b3c81d76380a17055696b
Jdam <jonathandalvarez7@gmail.com>
2025-07-25 23:44:12 -0500

    feat: :sparkles: add services logic

---
+++ services/transaction.service.ts
@@ -0,0 +1,52 @@
+// services/transaction.service.ts
+import { prisma } from "@/lib/auth/prisma";
+
+type NewTransactionInput = {
+    amount: number;
+    concept: string;
+    date: string;
+    userId: string;
+    type: "INCOME" | "EXPENSE";
+  };
+
+//get all transactions
+export async function getTransactionsService(userId?: string) {
+  return await prisma.transaction.findMany({
+    where: userId ? { userId } : undefined,
+    orderBy: { date: "desc" },
+    select: {
+      id: true,
+      concept: true,
+      amount: true,
+      date: true,
+      user: {
+        select: {
+          name: true,
+          email: true,
+        }
+      }
+    }
+  });
+}
+
+//create transactions
+export async function createTransactionService(data: NewTransactionInput) {
+    return await prisma.transaction.create({
+      data,
+      select: {
+        id: true,
+        concept: true,
+        amount: true,
+        date: true,
+        type: true,
+        userId: true,
+      },
+    });
+}
+
+//delete
+export async function deleteTransactionService(id: string) {
+  return await prisma.transaction.delete({
+    where: { id },
+  });
+}
---
+++ services/user.service.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/auth/prisma"
+
+type UpdateUserInput = {
+  name?: string;
+  role?: "ADMIN" | "USER";
+  email?: string;
+};
+
+//Get all users service
+export async function getAllUsersService() {
+  return await prisma.user.findMany({
+    select: { id: true, name: true, email: true, role: true },
+  });
+}
+
+//Edit user service by id
+export async function editUserByIdService(userId: string, data: UpdateUserInput) {
+  return await prisma.user.update({
+    where: { id: userId },
+    data,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+}
